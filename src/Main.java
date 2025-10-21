8917e7bc0012ba9c4efb1110a72be07679062ce1
victorcsarsampaio <tinhocsar@hotmail.com>
2025-10-21 12:03:51 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// Crie um programa que declare uma variável chamada nome do tipo String e outra chamada idade do tipo int.
+// Atribua valores a elas e exiba na tela: "Meu nome é [nome] e tenho [idade] anos."
+
+// Declare uma variável do tipo double chamada altura e atribua o valor 1.75.
+// Mostre na tela: "Minha altura é [altura] metros."
+
+
+public class Main {
+    public static void  main (String[] args) {
+        int idade = 30;
+        long quantidadeCachorros = 3L;
+        double salarioMensal = 3500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+    System.out.println("Idade: " + idade);
+    System.out.println("Quantos cachorros ? " + quantidadeCachorros);
+    System.out.println("Salário mensal: " + salarioMensal);
+    System.out.println("Sexo: " + genero);
+    System.out.println("Empregado Java ? " + empregadoJava);
+
+
+    }
+
+}
