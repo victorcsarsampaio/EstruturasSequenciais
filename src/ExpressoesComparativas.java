8b8b05133450fee157fc079dd1f63bce3a2c4fff
victorcsarsampaio <tinhocsar@hotmail.com>
2025-10-26 22:44:44 -0300

    🎉 ExpressoesComparativas.java

---
+++ src/ExpressoesComparativas.java
@@ -0,0 +1,13 @@
+public class ExpressoesComparativas {
+    public static void main(String[] args) {
+        int pao = 10;
+        int acucar = 20;
+
+        System.out.println(pao == acucar);
+        System.out.println(pao != acucar);
+        System.out.println(pao > acucar);
+        System.out.println(pao < acucar);
+        System.out.println(pao >= acucar);
+        System.out.println(pao <= acucar);
+    }
+}
