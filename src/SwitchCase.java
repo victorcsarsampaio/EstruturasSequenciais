5c88ddab55f23ae84231353b0b5fbff6ffea0fc6
victorcsarsampaio <tinhocsar@hotmail.com>
2025-11-01 11:25:08 -0300

    🎉 SwitchCase.java

---
+++ src/SwitchCase.java
@@ -0,0 +1,29 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int diaDeSemana = 5;
+
+        switch (diaDeSemana) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+        }
+    }
+}
