507ff32a220f79d5807a9781443941e9d014d6e9
Victor Sampaio <tinhocsar@hotmail.com>
2025-11-03 21:26:36 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void  main (String[] args) {
-        int idade = 30;
-        long quantidadeCachorros = 3L;
-        double salarioMensal = 3500.00;
-        char genero = 'M';
-        boolean empregadoJava = true;
+    //  int idade = 30;
+    //  long quantidadeCachorros = 3L;
+    //  double salarioMensal = 3500.00;
+    //  char genero = 'M';
+    //  boolean empregadoJava = true;
 
-    System.out.println("Idade: " + idade);
-    System.out.println("Quantos cachorros ? " + quantidadeCachorros);
-    System.out.println("Salário mensal: " + salarioMensal);
-    System.out.println("Sexo: " + genero);
-    System.out.println("Empregado Java ? " + empregadoJava);
+    //   System.out.println("Idade: " + idade);
+    //   System.out.println("Quantos cachorros ? " + quantidadeCachorros);
+    //   System.out.println("Salário mensal: " + salarioMensal);
+    //   System.out.println("Sexo: " + genero);
+    //   System.out.println("Empregado Java ? " + empregadoJava);
+
+        
+    //  String nome = "Victor Sampaio";
+    //  int idade = 30;
+    //  double peso = 70.3;
+
+    //  System.out.println("Qual seu nome : " + nome);
+    //  System.out.println("Qual sua idade: " + idade);
+    //  System.out.println("Qual o seu peso: " + peso);
+
 
     }
 }
